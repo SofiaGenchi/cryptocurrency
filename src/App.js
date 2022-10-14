00df511dd1d7d5fcbdb947fa2ffc4a936f06e6e3
Sofia Genchi <sofigen16@gmail.com>
2022-10-14 12:09:40 -0300

    app css

--- src/App.js
+++ src/App.js
@@ -6,42 +6,54 @@ import Coin from './components/Coin';
 
 
 function App() {
-  const [coins, setCoins] = useState([])
-  const [search, setSearch] = useState('')
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res => {
-      setCoins(res.data);
-    })
-    .catch(error => console.log(error))
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
   }, []);
 
   const handleChange = e => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
-  const filteredCoins = coins.filter(coin => 
+  const filteredCoins = coins.filter(coin =>
     coin.name.toLowerCase().includes(search.toLowerCase())
   );
 
   return (
-    <div className="coin-app">
+    <div className='coin-app'>
       <div className='coin-search'>
         <h1 className='coin-text'>Search a currency</h1>
         <form>
-          <input type='text' className='coin-input' placeholder='Search' onChange={handleChange}/>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
         </form>
       </div>
       {filteredCoins.map(coin => {
         return (
-          <Coin 
-          key={coin.id} 
-          name={coin.name} 
-          image={coin.image} 
-          symbol={coin.symbol} 
-          volume={coin.market_cap} 
-          price={coin.current_price} />
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
         );
       })}
     </div>
